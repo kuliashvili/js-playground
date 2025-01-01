b9f99293e4d74df557bb12885e3f92af61d93e1b
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2025-01-01 19:37:52 +0400

    array join add/remove basics js

--- arrays/basics.js
+++ arrays/basics.js
@@ -7,11 +7,22 @@ const fromConstructor = new Array(3);
 const fromValue = Array.from("giorgi");
 const spread = [...numbers];
 
-
 // 2. Array Access
 
 const firstElement = numbers[0];
 const lastElement = numbers[numbers.length - 1];
 const slicedArray = numbers.slice(1, 3);
 const includes = numbers.includes(5);
-const indexOf = numbers.indexOf(4)
+const indexOf = numbers.indexOf(4);
+
+// 3. Array Basic Methods add/remove/joining
+
+numbers.push(6);
+numbers.unshift(0);
+numbers.splice(1, 0, 999);
+const lastItem = number.pop();
+const firstItem = numbers.shift();
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2, 3];
+const joined = arr1.concat(arr2);
+const joinedWithSpreat = [...arr1, ...arr2];
