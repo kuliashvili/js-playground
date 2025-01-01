0d17924590392e59afe062933f9795f87786ead3
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2025-01-01 19:04:14 +0400

    array Access

--- arrays/basics.js
+++ arrays/basics.js
@@ -6,3 +6,12 @@ const mixed = ["nina", 33, { name: "gio" }, [1, 2, 3], true, false];
 const fromConstructor = new Array(3);
 const fromValue = Array.from("giorgi");
 const spread = [...numbers];
+
+
+// 2. Array Access
+
+const firstElement = numbers[0];
+const lastElement = numbers[numbers.length - 1];
+const slicedArray = numbers.slice(1, 3);
+const includes = numbers.includes(5);
+const indexOf = numbers.indexOf(4)
