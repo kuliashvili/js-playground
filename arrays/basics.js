a61cc688c4bfd67daaa30e0ea211e32a438a22a2
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2025-01-01 18:57:29 +0400

    Array creations

---
+++ arrays/basics.js
@@ -0,0 +1,8 @@
+// 1. Array Creation
+
+const empty = [];
+const numbers = [1, 2, 3, 4, 5];
+const mixed = ["nina", 33, { name: "gio" }, [1, 2, 3], true, false];
+const fromConstructor = new Array(3);
+const fromValue = Array.from("giorgi");
+const spread = [...numbers];
