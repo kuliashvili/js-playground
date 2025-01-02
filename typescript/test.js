5d0afaf60ffb7369ef5d4420e1f67176291cb18a
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2025-01-03 00:57:28 +0400

    testing typescript basics, greeting function

--- typescript/test.js
+++ typescript/test.js
@@ -1,3 +1,15 @@
 var firstUser = "giorgi";
 var helloUser = "Hello ".concat(firstUser);
 console.log(helloUser);
+var surname = "kuliashvili";
+var usersAge = 18;
+var isVerified = true;
+function greetingUser(a, b, c) {
+    var text = "hello dear ".concat(a, ", you are ").concat(b, " years old verified status: ").concat(c, " zd all");
+    return console.log(text.toUpperCase());
+}
+function printCoordinates(x, y) {
+    return console.log(x * 10, y * 10);
+}
+greetingUser(surname, usersAge, isVerified);
+printCoordinates(10, 29);
--- typescript/test.ts
+++ typescript/test.ts
@@ -1,4 +1,20 @@
-let firstUser: String = "giorgi"
-let helloUser: string = `Hello ${firstUser}`
+let firstUser: String = "giorgi";
+let helloUser: string = `Hello ${firstUser}`;
 
-console.log(helloUser)
+console.log(helloUser);
+
+let surname: string = "kuliashvili";
+let usersAge: number = 18;
+let isVerified: boolean = true;
+
+function greetingUser(a: string, b: number, c: boolean) {
+  let text: string = `hello dear ${a}, you are ${b} years old verified status: ${c} zd all`;
+  return console.log(text.toUpperCase());
+}
+
+function printCoordinates(x: number, y: number) {
+  return console.log(x * 10, y * 10);
+}
+
+greetingUser(surname, usersAge, isVerified);
+printCoordinates(10, 29);
