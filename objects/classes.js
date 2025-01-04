3f41be62c0054b33768f23d2a0e053e8d9460865
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2025-01-04 11:15:43 +0400

    product class example in objects

--- objects/classes.js
+++ objects/classes.js
@@ -29,3 +29,38 @@ const user1 = new User("giorgi", "gio@gmail.com", 22);
 user1.login();
 user1.getinfo();
 console.log(user1.getinfo());
+
+// Product class - Handle product details
+
+class Product {
+  constructor(name, price, stock) {
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  checkstock() {
+    return this.stock > 0;
+  }
+
+  updatePrice(newPrice) {
+    this.price = newPrice;
+  }
+
+  sell(quantity) {
+    if (quantity >= this.stock) {
+      return `We dont have that much item, we only have ${this.stock} items left`;
+    } else {
+      this.stock = this.stock - quantity;
+      return `Nice, we sold ${quantity} items of ${this.name}, we have ${this.stock} ${this.name} left`;
+    }
+  }
+}
+
+// Usage
+
+const product1 = new Product("banana", 4.99, 100);
+product1.checkstock();
+product1.updatePrice(3);
+
+console.log(product1.sell(200));
