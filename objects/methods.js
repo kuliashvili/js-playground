86856e97e3eed4e7dcb870ab2e6283ee0c38cf03
Giorgi Kuliashvili <giorgi.kuliashvili.1@btu.edu.ge>
2025-01-05 17:42:55 +0400

    object methods in js operations/ access

--- objects/methods.js
+++ objects/methods.js
@@ -11,3 +11,34 @@ Object.freeze(person); //makes object immutable
 Object.preventExtensions(person); //prevents adding new properties
 
 console.log(person);
+
+// Getting Object information
+
+Object.keys(person); //returns array of property names
+Object.values(person); //returns array of values
+Object.entries(person); //returns array of [key: value]
+Object.getOwnPropertyNames(person); //return array of propertys
+
+//Checking object state
+Object.is(person, otherObject); //compares two values to equality
+Object.isSealed(person); //checks if sealed
+Object.isFrozen(person); //check if frozen
+Object.isExtensible(person); //checks if new things can be added
+
+// property operations
+
+person.hasOwnProperty("name"); // checks if property exists in object
+person.toString(); //returns string representation
+person.valueOf(); //returns primitive value
+
+//Object destructuring
+
+const { car, workingHours } = person; // extract properties into variables
+
+//property access
+
+person["name"]; //bracket notation
+person.name; //dot notation
+
+// property existence check
+"name" in person; //checks if property exists in person
